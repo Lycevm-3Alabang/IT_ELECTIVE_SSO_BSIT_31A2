4d9666ac84a60e64a31213ba0dd6a2712559987e
Renen <Renen@DESKTOP-1B94TU9>
2026-08-28 03:15:16 +0800

    Added SsoDbContext Base

---
+++ Data/SsoDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ITElectiveSSO.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITELECTIVE_SSO.Data
+{
+    public class SsoDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public SsoDbContext(DbContextOptions<SsoDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
