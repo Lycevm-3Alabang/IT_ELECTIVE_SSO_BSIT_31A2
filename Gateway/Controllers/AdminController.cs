f6a27343eb3af56ce36d888bdcf6778c38f45b4c
markjustinfactor213-ops <markjustinfactor213@gmail.com>
2026-08-29 21:51:46 +0800

    added admin controller, _auditlogstable and view

---
+++ Gateway/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SSO_Gateway.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.HasData = false;
+
+            ViewBag.TotalUsers = 0;
+            ViewBag.ActiveApps = 0;
+            ViewBag.AssignedGroups = 0;
+            ViewBag.AuditEvents = 0;
+
+            return View();
+        }
+
+        public IActionResult Users() => View();
+        public IActionResult Apps() => View();
+        public IActionResult Groups() => View();
+        public IActionResult AuditLogs() => View();
+    }
+}
