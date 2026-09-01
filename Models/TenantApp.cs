c094e8689d76f3dd11ae3e7d4d1043122b7eed2a
Renen <Renen@DESKTOP-1B94TU9>
2026-09-01 23:33:47 +0800

    Refactored Fluent Configuration

--- Models/TenantApp.cs
+++ Models/TenantApp.cs
@@ -6,13 +6,8 @@ namespace ITElectiveSSO.Models
     public class TenantApp
     {
         public int Id { get; set; }
-
-        [Required, MaxLength(100)]
         public string Name { get; set; } = string.Empty;
-
-        [Required, MaxLength(500)]
         public string ReturnUrl { get; set; } = string.Empty;
-
         public bool IsActive { get; set; } = true;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
