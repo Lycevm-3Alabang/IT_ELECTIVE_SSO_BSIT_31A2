ae92c75139b4bbeaa9bf62844a70a042943552e3
Renen <Renen@DESKTOP-1B94TU9>
2026-09-10 20:40:27 +0800

    Fixed Empty

--- Gateway/Areas/Admin/Controllers/TenantAppsController.cs
+++ Gateway/Areas/Admin/Controllers/TenantAppsController.cs
@@ -3,17 +3,3 @@ using ITELECTIVE_SSO.Data;
 using ITElectiveSSO.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-
-namespace Gateway.Areas.Admin.Controllers
-{
-    [Area("Admin")]
-    public class TenantAppsController : Controller
-    {
-        private readonly SsoDbContext _context;
-
-        public TenantAppsController(SsoDbContext context)
-        {
-            _context = context;
-        }
-    }
-}
