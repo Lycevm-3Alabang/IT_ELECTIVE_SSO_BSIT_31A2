f259c8f9c5a8b5c613d15eb6f32ed6a394f6a7f2
Renen <Renen@DESKTOP-1B94TU9>
2026-09-10 12:37:33 +0800

    Added TenantApps Controller & ViewModel

---
+++ Gateway/Areas/Admin/Controllers/TenantAppsController.cs
@@ -0,0 +1,22 @@
+﻿using Gateway.Areas.Admin.Models;
+using ITELECTIVE_SSO.Data;
+using ITElectiveSSO.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gateway.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TenantAppsController : Controller
+    {
+        private readonly SsoDbContext _context;
+
+        public TenantAppsController(SsoDbContext context)
+        {
+            _context = context;
+        }
+
+        // Tasks 2-9
+
+    }
+}
