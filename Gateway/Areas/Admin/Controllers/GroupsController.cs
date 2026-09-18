207f0f318b6f12fe26bab958b2ed8accf16e136d
Renen <Renen@DESKTOP-1B94TU9>
2026-09-18 19:00:22 +0800

    Added Groups Controller (Admin Area)

---
+++ Gateway/Areas/Admin/Controllers/GroupsController.cs
@@ -0,0 +1,34 @@
+﻿using Gateway.Areas.Admin.Models;
+using ITELECTIVE_SSO.Data;
+using ITElectiveSSO.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gateway.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class GroupsController : Controller
+    {
+        private readonly SsoDbContext _context;
+
+        public GroupsController(SsoDbContext context)
+        {
+            _context = context;
+        }
+
+        // TASK 2
+
+        // TASK 3
+
+        // TASK 4 
+
+        // TASK 5
+
+        // TASK 6
+
+        // TASK 7
+
+        // TASK 9
+    }
+}
---
+++ Gateway/Areas/Admin/Models/GroupViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Gateway.Areas.Admin.Models
+{
+    public class GroupViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please select an app.")]
+        [Display(Name = "Tenant App")]
+        public int TenantAppId { get; set; }
+
+        [Required(ErrorMessage = "Group name is required.")]
+        [StringLength(100, ErrorMessage = "Group name cannot exceed 100 characters.")]
+        [Display(Name = "Group Name")]
+        public string Name { get; set; } = string.Empty;
+
+        // TASK 8
+        public List<SelectListItem>? TenantApps { get; set; }
+    }
+}
